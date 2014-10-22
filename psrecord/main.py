3ec5263e059847aac182ef2edc16e32dd7382cf7
Thomas Robitaille <thomas.robitaille@gmail.com>
2014-10-22 11:31:44 +0100

    Make change backwards-compatible

--- psrecord/main.py
+++ psrecord/main.py
@@ -117,8 +117,13 @@ def monitor(pid, logfile=None, plot=None, duration=None, interval=None, include_
         # Find current time
         current_time = time.time()
 
+        try:
+            pr_status = pr.status()
+        except TypeError:  # psutil < 2.0
+            pr_status = pr.status
+
         # Check if process status indicates we should exit
-        if pr.status() in [psutil.STATUS_ZOMBIE, psutil.STATUS_DEAD]:
+        if pr_status in [psutil.STATUS_ZOMBIE, psutil.STATUS_DEAD]:
             print("Process finished ({:.2} seconds)"
                   .format(current_time - start_time))
             break
