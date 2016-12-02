b4cb3154c809840b66f74b259b6d7d2ddd49926c
Thomas Robitaille <thomas.robitaille@gmail.com>
2016-12-02 16:25:59 +0000

    Added test to make sure things work with sub-processes

--- psrecord/tests/test_main.py
+++ psrecord/tests/test_main.py
@@ -2,7 +2,32 @@ import os
 import sys
 import subprocess
 
-from ..main import main, monitor
+import psutil
+from ..main import main, monitor, all_children
+
+TEST_CODE = """
+import subprocess
+p = subprocess.Popen('sleep 5'.split())
+p.wait()
+"""
+
+
+def test_all_children(tmpdir):
+
+    filename = tmpdir.join('test.py').strpath
+
+    with open(filename, 'w') as f:
+        f.write(TEST_CODE)
+
+    p = subprocess.Popen('{0} {1}'.format(sys.executable, filename).split())
+
+    import time
+    time.sleep(1)
+
+    pr = psutil.Process(p.pid)
+    children = all_children(pr)
+    assert len(children) > 0
+    p.kill()
 
 
 class TestMonitor(object):
@@ -18,6 +43,10 @@ class TestMonitor(object):
 
     def test_simple_with_interval(self):
         monitor(self.p.pid, duration=3, interval=0.1)
+
+    def test_with_children(self, tmpdir):
+        # Test with current process since it has a subprocess (self.p)
+        monitor(os.getpid(), duration=3, include_children=True)
 
     def test_logfile(self, tmpdir):
         filename = tmpdir.join('test_logfile').strpath
