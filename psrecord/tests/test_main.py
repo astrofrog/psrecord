299b4674ed424e185e5023204b28107fbf91718d
Thomas Robitaille <thomas.robitaille@gmail.com>
2024-04-25 23:24:23 +0100

    Skip test on Mac

--- psrecord/tests/test_main.py
+++ psrecord/tests/test_main.py
@@ -69,5 +69,6 @@ class TestMonitor(object):
         sys.argv = ['psrecord', '--duration=3', str(os.getpid())]
         main()
 
+    @pytest.mark.skipif(sys.platform == 'darwin', reason="Functionality not supported on MacOS")
     def test_io(self, tmpdir):
         monitor(os.getpid(), duration=3, include_io=True)
